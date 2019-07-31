517ad0be5ab86d02a661e3dcdfbe6231ae146638
Artem Bursuk <a.bursuk@gmail.com>
2019-08-01 01:49:32 +0300

    grayscale

--- StaticLib/Shaders/PixelShaderPP.hlsl
+++ StaticLib/Shaders/PixelShaderPP.hlsl
@@ -9,6 +9,9 @@ struct PS_INPUT
 float4 main(PS_INPUT input) : SV_TARGET
 {
 	float4 color = offScreenTexture.Sample(simpleSamplerPP, input.textCoord);
+    float greyscaleAverage = (color.r + color.g + color.b) / 3.0f;
+    color.rgba = float4(greyscaleAverage, greyscaleAverage, greyscaleAverage, color.a);
+
 
 	return color;
 }
